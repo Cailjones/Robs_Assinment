551640bf0e8c6e8d909071ab3ec012a0e301d61e
Cailjones <57351045+Cailjones@users.noreply.github.com>
2022-01-21 02:04:52 +0000

    Making progression on code loops and narrative pogeression

--- Assinment1_Code/Assinment1_Code/Assinment1_Code.cpp
+++ Assinment1_Code/Assinment1_Code/Assinment1_Code.cpp
@@ -2,19 +2,78 @@
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string adveturesName, YesOrNO, Choice, Weapon, GreatAxe, GoldenSward, MagicalWond, fistsOfFury;
+	int WeaponOfChoice;
+	//bool again = true;// to allow the progrmam to rename the charactor
+
+	cout << "-----------------\n";//title
+	cout << "-Adventure-Quest-\n";//title
+	cout << "-----------------\n";//title
+
+	cout << "What's your advetures name?\n";//Characters neme being collected
+	cin >> adveturesName;//Charaters name Input
+
+	//cout << "would you like to continue with" << adveturesName << " as your name? y/n";//Asking if the namme they put in is correct
+	//cin >> YesOrNO;//Answer y/n
+
+	/*while (again);
+	{
+
+
+		if (YesOrNO == "y" || YesOrNO == "Y")
+		{
+			cout << "lets continue Shall we\n";// continues with the progeamm after the loop
+
+		}
+		else if (YesOrNO == "N" || YesOrNO == "n")
+		{
+
+			return again;
+		}
+	}
+	*/
+
+	cout << "You have made it " << adveturesName << " lets start the quest shall we.\n";//Narrative
+	cout << "You hesetently agree but its only the beging.";//Narrative
+	cout << "The next morning\n";//Narrative
+	cout << "You wake up in a small town, not knowing of the quest that lies ahead!\n";//Narrative
+
+	cout << adveturesName << " Your awake, now lets get you some gear.\n";//Narrative
+	cout << adveturesName << " Your lucky we haev a fine choice of weapons for you!\n";
+
+	cout << "Now chose between all 3 of theses fine weapons (GreatAxe[1], GoldenSward[2], MagicalWond[3] ).\n";
+	cin >> WeaponOfChoice;//WeaponOfChoice input
+
+	if (WeaponOfChoice == 1)//loop for each choice(GreatAxe)
+	{
+		cout << "You have chosen the GreatAxe what a fine choice.\n";
+		Weapon = GreatAxe;
+
+	}
+	else if (WeaponOfChoice == 2)//loop for each choice(GoldenSward)
+	{
+		cout << "You have chosen the GoldenSward an adventure of fine tast i see.\n";
+		Weapon = GoldenSward;
+	}
+	else if (WeaponOfChoice == 3)//loop for each choice(MagicalWond)
+	{
+		cout << "You have chosen the MagicalWond, i see your a mage wanna be very well so be it.\n";
+		Weapon = MagicalWond;
+	}
+	else
+	{
+		cout << "i see you dont need a weapon, very well so be it\n";
+		Weapon = fistsOfFury;
+	}
+
+	cout << "let us continue, this way!!";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
