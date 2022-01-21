d18b431c39dadb7c748af72dc023c1e7a64fab91
Cailjones <57351045+Cailjones@users.noreply.github.com>
2022-01-21 23:14:49 +0000

    Made changes to both while loops as well as adding a game reset (needs fixing)

--- Assinment1_Code/Assinment1_Code/Assinment1_Code.cpp
+++ Assinment1_Code/Assinment1_Code/Assinment1_Code.cpp
@@ -2,17 +2,20 @@
 //
 
 #include <iostream>
-#include <iostream>
+
 using namespace std;
 
+void GameReset()
+{
+	main();
+}
 
 int main()
 {
-	string adveturesName, YesOrNO, Choice, Weapon, GreatAxe, GoldenSward, MagicalWond, fistsOfFury, action;
-	int WeaponOfChoice;
-	char again = 'Y' || 'y';
-	char loop = 1 || 2 || 3 ;
-
+	string adveturesName, YesOrNO, Choice, Weapon, GreatAxe, GoldenSward, MagicalWond, fistsOfFury, action;//strings
+	//string Weapons[3]{ "GreateAxe", "GoldenSward", "MagicalWond" };
+	int WeaponOfChoice;// intragers
+	char again;//stores the charrcter for the while loop
 
 	cout << "-----------------\n";//title
 	cout << "-Adventure-Quest-\n";//title
@@ -21,12 +24,10 @@ int main()
 	cout << "What's your advetures name?\n";//Characters neme being collected
 	cin >> adveturesName;//Charaters name Input
 
-	cout << "would you like to continue with " << adveturesName << " as your name? y/n\n";//Asking if the namme they put in is correct
-	cin >> again;//Answer y/n||Y\N
-
-
-	while (again)
+	while (true)
 	{
+		cout << "would you like to continue with " << adveturesName << " as your name? y/n\n";//Asking if the namme they put in is correct
+		cin >> again;//Answer y/n||Y\N
 
 		if(again == 'N' || again == 'n')
 		{
@@ -35,9 +36,11 @@ int main()
 
 			cout << "would you like to continue with " << adveturesName << " as your name? y/n";//Asking if the namme they put in is correct
 			cin >> again;//Answer y/n||
+			break;
 		}
-		if (again == 'Y' || again == 'y')
+		else if (again == 'Y' || again == 'y')
 		{
+			cout << "Lets continue shall we";
 			break;
 		}
 
@@ -59,15 +62,24 @@ int main()
 	cout << adveturesName << " Your awake, now lets get you some gear.\n";//Narrative
 	cout << adveturesName << " Your lucky we haev a fine choice of weapons for you!\n";
 
-	cout << "Now chose between all 3 of theses fine weapons (GreatAxe[1], GoldenSward[2], MagicalWond[3] ).\n";
-	cin >> WeaponOfChoice;//WeaponOfChoice input
-	
-	while(loop == 1)
+	/*do
 	{
+		while(Weapons)
+		{
+
+		}
+	}
+	*/
+	while (true)
+	{
+		cout << "Now chose  between all 3 of theses fine weapons (GreatAxe[1], GoldenSward[2], MagicalWond[3] ).\n";
+		cin >> WeaponOfChoice;
+
 		if (WeaponOfChoice == 1)//loop for each choice(GreatAxe)
 		{
 			cout << "You have chosen the GreatAxe what a fine choice.\n";
 			Weapon = GreatAxe;
+            break;
 		
 
 		}
@@ -75,24 +87,23 @@ int main()
 		{
 			cout << "You have chosen the GoldenSward an adventure of fine tast i see.\n";
 			Weapon = GoldenSward;
+			break;
 		}
 		else if (WeaponOfChoice == 3)//loop for each choice(MagicalWond)
 		{
 			cout << "You have chosen the MagicalWond, i see your a mage wanna be very well so be it.\n";
 			Weapon = MagicalWond;
-		}
-		else(WeaponOfChoice);
-		{
-			cout << "i see your being stubben but you must chose a weopon \n";
-
-			cout << "Now chose between all 3 of theses fine weapons (GreatAxe[1], GoldenSward[2], MagicalWond[3] ).\n";
-			cin >> WeaponOfChoice;//WeaponOfChoice input
-			if (WeaponOfChoice == )
 			break;
 		}
+		else(WeaponOfChoice);//if the statment is incorrect 
+		{
+			cout << "i see your being stubben but you must chose a weopon \n";
+		}
 	}
+	
 	cout << "let us continue, this way!!";
 
+    GameReset();
 }
 
 
